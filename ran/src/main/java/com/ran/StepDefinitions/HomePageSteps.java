99a495f84d03f9870e5e2ccb04b322f1ad17f8ff
ranjith236 <qa.ranjithchatharaju@gmail.com>
2014-06-22 17:17:01 +0100

    added utility to master

---
+++ ran/src/main/java/com/ran/StepDefinitions/HomePageSteps.java
@@ -0,0 +1,18 @@
+package com.ran.StepDefinitions;
+
+public class HomePageSteps {
+/*	
+
+@Given("^I navigate to the URL$")
+public void i_navigate_to_the_URL() throws Throwable {
+    
+}
+
+@Then("^I Verify the page is displayed$")
+public void i_Verify_the_page_is_displayed() throws Throwable {
+    
+}
+
+*/
+
+}
---
+++ ran/src/main/java/com/ran/utils/Browsers.java
@@ -0,0 +1,21 @@
+package com.ran.utils;
+
+public enum Browsers {
+
+	FIREFOX,
+	CHROME,
+	SAFARI;
+
+	public static Browsers browserForName(String browser) throws IllegalArgumentException{
+        for(Browsers b: values()){
+    		if(b.toString().equalsIgnoreCase(browser)){
+    			return b;
+    		}
+        }
+        throw browserNotFound(browser);
+    }
+
+    private static IllegalArgumentException browserNotFound(String outcome) {
+        return new IllegalArgumentException(("Invalid browser [" + outcome + "]"));
+    }
+}
